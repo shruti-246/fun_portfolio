395b0149c7a73a0e0e694fccae2d6015399a160d
Shruti Debnath <debnathshruti477@gmail.com>
2025-09-22 11:29:56 -0700

    Remove extra Router wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/landing";
 import Recruiter from "./pages/Recruiter";
 import Skills from "./pages/Skill";
