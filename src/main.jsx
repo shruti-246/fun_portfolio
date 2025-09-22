7191389ce89de7c3c480493e5207ceda42a6b227
Shruti Debnath <debnathshruti477@gmail.com>
2025-09-22 11:26:17 -0700

    Fix router & vite base for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,14 @@
 // main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import Landing from "./pages/landing";
-import Recruiter from "./pages/Recruiter";
-import Skills from "./pages/Skill";
-import Projects from "./pages/Proj";
-import Experience from "./pages/Exp";
-import Certifications from "./pages/Cert";
-import Contact from "./pages/Cont";
-import WorkPermit from "./pages/Work_permit";
-import Recommendations from "./pages/Recom";
-
-const router = createBrowserRouter([
-  { path: "/", element: <Landing /> },
-  { path: "/recruiter", element: <Recruiter /> },
-  { path: "/skills", element: <Skills /> },
-  { path: "/projects", element: <Projects /> },
-  { path: "/experience", element: <Experience /> },
-  { path: "/certifications", element: <Certifications /> },
-  { path: "/contact", element: <Contact /> },
-  { path: "/work-permit", element: <WorkPermit /> },
-  { path: "/recommendations", element: <Recommendations /> },
-]);
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter basename="/fun_portfolio">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
